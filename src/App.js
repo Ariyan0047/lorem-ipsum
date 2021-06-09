7c68b379d9eebd42bcf2bef1a2946acfbd4fccad
Ariyan0047 <aariyan0047@gmail.com>
2021-06-09 13:45:43 +0600

    form & style added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "./data";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([0]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="container">
-      <h1>hello</h1>
+    <div className="App">
+      <div className="container text-center">
+        <h1>generate lorem ipsum</h1>
+      </div>
+      <section className="container section text-center mt-2">
+        <form className="form" onSubmit={handleSubmit}>
+          <span className="para">paragraphs</span>
+          <input
+            name="num"
+            type="number"
+            value={count}
+            onChange={(event) => setCount(event.target.value)}
+          />
+          <input type="button" value="generate" />
+        </form>
+      </section>
     </div>
   );
 };
