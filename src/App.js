e69ceaacd780ba6c182375314197a18fe3afe3fb
Ariyan0047 <aariyan0047@gmail.com>
2021-06-09 15:10:21 +0600

    display data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
 import { data } from "./data";
+import { v4 as uuid } from "uuid";
+import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState([0]);
+  const [text, setText] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setText(data);
   };
 
   return (
@@ -14,7 +16,7 @@ const App = () => {
       <div className="container text-center">
         <h1>generate lorem ipsum</h1>
       </div>
-      <section className="container section text-center mt-2">
+      <section className="container section1 text-center">
         <form className="form" onSubmit={handleSubmit}>
           <span className="para">paragraphs</span>
           <input
@@ -23,8 +25,13 @@ const App = () => {
             value={count}
             onChange={(event) => setCount(event.target.value)}
           />
-          <input type="button" value="generate" />
+          <input type="button" value="generate" className="btn btn-warning" />
         </form>
+      </section>
+      <section className="container section2 text-center">
+        {text.map((item) => {
+          return <p key={uuid()}>{item}</p>;
+        })}
       </section>
     </div>
   );
